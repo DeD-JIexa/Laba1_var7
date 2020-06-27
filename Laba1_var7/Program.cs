bc29aadbe756fdcb24e779f42842a1de147112a1
Alex <alex3kot@outlook.com>
2020-06-28 01:17:26 +0400

    Добавьте файлы проекта.

---
+++ Laba1_var7/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Laba1_var7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число:");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine((num == 0) || (reverse(num, 0) == num));
+            Console.ReadKey(true);
+        }
+        private static int reverse(int num, int acc)
+        {
+            while (num > 0) { acc = acc * 10 + num % 10; num /= 10; }
+            return acc;
+        }
+    }
+}
